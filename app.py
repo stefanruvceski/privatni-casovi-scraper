aef952480731589f7457987953aab94f2b7ce307
Stefan Ruvceski <stefanruvceski@gmail.com>
2021-02-23 01:35:52 +0100

    final

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask ,jsonify,render_template
+from flask import Flask ,jsonify,render_template,request
 from selenium import webdriver
 import os
 from casovi_scraper import login, get_info
 app = Flask(__name__)
 
-@app.route("/login")
-def dojob():
-    login()
-    return jsonify({'response' : 'ok'}),200
-
-@app.route('/getinfo/<token>')
-def getinfo(token):
+@app.route("/login",methods = ['POST', 'GET'])
+def login():
     try:
-        result = get_info(token)
-        return render_template("index.html", names=result)
+        email = request.form['email']
+        login(email)
+        return render_template("token.html")
     except:
-        return jsonify({'response' : token}),200
+        return render_template("error.html",err='Doslo je do greske prilikom prijave')
+
+@app.route("/getstats",methods = ['POST', 'GET'])
+def getinfo():
+    try:
+        token = request.form['token']
+        my_name,stats = get_info(token)
+        return render_template("index.html", stats=stats, my_name=my_name)
+    except:
+        return render_template("error.html",err='Doslo je do greske prilikom kreiranja statistike')
+    
 @app.route("/")
 def index():
-    return jsonify({'login': '/login'},{'getinfo':'/getlist/url'}),200
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- casovi_scraper.py
+++ casovi_scraper.py
@@ -9,13 +9,15 @@ options.add_argument('--headless')
 options.add_argument('--no-sandbox')   
 options.add_argument('--disabele-dev-sh-usage')   
 driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
-    
-def login():
+
+# driver = webdriver.Chrome()
+
+def login(emaildata):
     url = 'https://privatni-casovi.net/sessions/new'
     driver.get(url)
     time.sleep(1)
     email = driver.find_element_by_id('email-login-input')
-    email.send_keys("stefanruvceski@gmail.com")
+    email.send_keys(emaildata)
 
     driver.find_element_by_xpath('/html/body/main/div/div/form/div[1]/div[2]/input').click()
 
@@ -24,16 +26,16 @@ def get_info(token):
     url = f"https://privatni-casovi.net/sessions/{token}/login"
     driver.get(url)
     time.sleep(1)
-    urls =  ['https://privatni-casovi.net/python/predmet',
-            'https://privatni-casovi.net/c/predmet' ,
-            'https://privatni-casovi.net/web-development/predmet' ,
-            'https://privatni-casovi.net/objektno-orijentisano-programiranje-java-c/predmet' ,
-            'https://privatni-casovi.net/baze-podataka/predmet' ,
-            'https://privatni-casovi.net/iz-programiranja/predmet' ,
-            'https://privatni-casovi.net/asp-net/predmet' ,
-            'https://privatni-casovi.net/iz-c-i-c-plus-plusa/predmet' ,
-            'https://privatni-casovi.net/informatika-i-racunarstvo/predmet'
-            ]
+    url = 'https://privatni-casovi.net/teacher'
+    driver.get(url)
+    
+    my_name = driver.find_element_by_class_name('profile-name').text
+    
+    time.sleep(1)
+    subjects = driver.find_elements_by_class_name('badge')
+    urls = []
+    for subject in subjects:
+        urls.append(subject.find_element_by_tag_name('a').get_attribute('href'))
     result = []
     for index in range(0,len(urls)):
 
@@ -45,10 +47,10 @@ def get_info(token):
         for profile in profiles:
             name = profile.find_element_by_class_name('name').find_element_by_tag_name('a').text;
             i+=1
-            if(name == 'Stefan Ruvceski'):
-                result.append(f'{name} je {i} na listi za predmet {urls[index].split("/")[3]}')
+            if name == my_name:
+                result.append(f'{i} na listi za predmet {urls[index].split("/")[3]}')
                 break
-    return result
+    
+    return my_name,result
 
-def quit():  
-    driver.quit()
+    
